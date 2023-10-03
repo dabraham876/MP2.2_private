4556ba979fb61d9661c85645859ce2831c9c655a
dabraham876 <78887828+dabraham876@users.noreply.github.com>
2023-10-02 20:22:14 -0500

    Update search_eval.py

--- search_eval.py
+++ search_eval.py
@@ -15,11 +15,11 @@ def load_ranker(cfg_file):
        
        okapiRanker=metapy.index.OkapiBM25(k1=1.25,b=0.8,k3=0)
     elif 'faculty' in cfg_file:
-       okapiRanker=metapy.index.OkapiBM25(k1=0.5,b=0.8,k3=500)
+       okapiRanker=metapy.index.OkapiBM25(k1=0.52,b=0.8,k3=500)
     elif 'news' in cfg_file:
-       okapiRanker=metapy.index.OkapiBM25(k1=.95,b=0.3,k3=500)
+       okapiRanker=metapy.index.OkapiBM25(k1=.93,b=0.3,k3=500)
     else:
-       okapiRanker=metapy.index.OkapiBM25(k1=1.25,b=0.8,k3=0)
+       okapiRanker=metapy.index.OkapiBM25(k1=1.20,b=0.8,k3=0)
         
     return okapiRanker     
     
